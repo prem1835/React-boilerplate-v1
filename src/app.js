53a3671e2e74de4cfa7e9f845c767a3eebaa6699
Prem Mali <premmali1835@gmail.com>
2020-07-03 13:25:27 +0530

    boilerplate-v1

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById("app"));
